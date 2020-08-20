859c55205782afe13f7130539028a5e795375a45
adam aji nugroho <adam.ajinugroho@yahoo.com>
2020-08-20 22:28:40 +0700

    add test

---
+++ WeatherAPI.Tests/WeatherForecastControllerTest.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using azure_cicd_test;
+using azure_cicd_test.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace WeatherAPI.Tests {
+    public class WeatherForecastTests : IDisposable {
+        WeatherForecastController controller;
+
+        public WeatherForecastTests () {
+
+            controller = new WeatherForecastController (null);
+        }
+
+        public void Dispose () {
+            controller = null;
+        }
+
+        [Fact]
+        public void GetWeatherForecastItemsReturnsNotNull () {
+            //Arrange
+
+            //Act
+            var result = controller.Get ();
+
+            //Assert
+            Assert.NotNull (result);
+        }
+
+    }
+}
